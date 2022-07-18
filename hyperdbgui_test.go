690b81a3bbce6767e8084bc26ad90d0491c5e93e
Admin <2762713521@qq.com>
2022-07-18 07:17:14 -0700

    start event work

--- hyperdbgui_test.go
+++ hyperdbgui_test.go
@@ -1,7 +1,7 @@
 package hyperdbgui
 
-//go:generate   go get github.com/ddkwork/librarygo@master
-//go:generate  go  clean -modcache
+//go:generate  go get github.com/ddkwork/librarygo@master
+//go:generate  go clean -modcache
 //go:generate  go work init
 //go:generate  go work use -r .
 //go:generate  go work sync
