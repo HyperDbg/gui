1f16b82372cf17854b1db2c9f5d57e358ab39b72
Admin <2762713521@qq.com>
2022-07-19 20:49:16 -0700

    update mod

--- cmd/hyperdbg/man.go
+++ cmd/hyperdbg/man.go
@@ -2,31 +2,19 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 	"github.com/ddkwork/hyperdbgui"
-	"github.com/ddkwork/hyperdbgui/src/fnTable/pageCpu"
 	"github.com/ddkwork/librarygo/src/fynelib/fyneTheme"
 )
 
-//go:embed 1786555.png
-var ico1 []byte
-
-//go:embed stock-illustration-debugger.jpg
-var ico2 []byte
-
-//go:generate  go build .
 func main() {
-	//main2()
-	//return
 	a := app.NewWithID("org.hyperdbg")
 	a.SetIcon(fyne.NewStaticResource("ico1", ico1))
 	fyneTheme.New().SetDarkTheme(a)
 	w := a.NewWindow("Hyper Debugger")
 	w.SetMaster()
+	w.SetPadded(false)
 	w.SetFullScreen(true)
 	h := hyperdbgui.New()
 	w.SetMainMenu(h.MainMenu())
@@ -35,50 +23,10 @@ func main() {
 	w.ShowAndRun()
 }
 
-func main2() {
-	a := app.New()
-	w := a.NewWindow("TEST")
-	tab := container.NewDocTabs()
-	tab.Append(&container.TabItem{
-		Text: "Test",
-		Content: container.NewMax(
-			pageCpu.New().CanvasObject(w),
-			//makeTableTab(w),
-		),
-	})
-	tab.Append(&container.TabItem{
-		Text: "Test2",
-		Content: container.NewMax(
-			pageCpu.New().CanvasObject(w),
-			//makeTableTab(w),
-		),
-	})
+//go:embed 1786555.png
+var ico1 []byte
 
-	w.SetContent(tab)
+//go:embed stock-illustration-debugger.jpg
+var ico2 []byte
 
-	w.Resize(fyne.NewSize(800, 500))
-	w.CenterOnScreen()
-	w.ShowAndRun()
-}
-
-func makeTableTab(_ fyne.Window) fyne.CanvasObject {
-	t := widget.NewTable(
-		func() (int, int) { return 500, 150 },
-		func() fyne.CanvasObject {
-			return widget.NewLabel("Cell 000, 000")
-		},
-		func(id widget.TableCellID, cell fyne.CanvasObject) {
-			label := cell.(*widget.Label)
-			switch id.Col {
-			case 0:
-				label.SetText(fmt.Sprintf("%d", id.Row+1))
-			case 1:
-				label.SetText("A longer cell")
-			default:
-				label.SetText(fmt.Sprintf("Cell %d, %d", id.Row+1, id.Col+1))
-			}
-		})
-	t.SetColumnWidth(0, 34)
-	t.SetColumnWidth(1, 102)
-	return t
-}
+//go:generate  go build .
--- src/fnTable/pageSymbol/symbolChoose/symbolChoose.go
+++ src/fnTable/pageSymbol/symbolChoose/symbolChoose.go
@@ -24,18 +24,38 @@ type (
 */
 
 func (o *object) CanvasObject(window fyne.Window) fyne.CanvasObject {
-	path := swid.NewTextFormField("path", "")
+	path := swid.NewTextFormField("symbol path", "")
 	load := widget.NewButtonWithIcon("load", nil, func() {})
 	reLoad := widget.NewButtonWithIcon("reLoad", nil, func() {})
-	local := widget.NewButtonWithIcon("local", nil, func() {})
+	local := widget.NewButtonWithIcon("local vmi", nil, func() {})
 	clientPipeStart := widget.NewButtonWithIcon("clientPipeStart", nil, func() {})
 	serverPipePort := swid.NewTextFormField("server Pipe Port", "")
-	return container.NewGridWithRows(3,
-		path,
+
+	ntoskrnl := widget.NewCheck("ntoskrnl", func(b bool) {})
+	win32k := widget.NewCheck("win32k", func(b bool) {})
+	win32kfull := widget.NewCheck("win32kfull", func(b bool) {})
+	fltmgr := widget.NewCheck("fltmgr", func(b bool) {})
+
+	ntdll := widget.NewCheck("ntdll", func(b bool) {})
+	kernel32 := widget.NewCheck("kernel32", func(b bool) {})
+	kernelbase := widget.NewCheck("kernelbase", func(b bool) {})
+	user32 := widget.NewCheck("user32", func(b bool) {})
+	//todo add iphelp mac dll
+
+	return container.NewGridWithColumns(4,
+		ntoskrnl,
+		win32k,
+		win32kfull,
+		fltmgr,
+		ntdll,
+		kernel32,
+		kernelbase,
+		user32,
 		load,
 		reLoad,
 		local,
 		clientPipeStart,
+		path,
 		serverPipePort,
 	)
 }
