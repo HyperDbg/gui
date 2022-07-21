76f5eb117f104b0c7ad7201056bcf7307bb38065
Admin <2762713521@qq.com>
2022-07-21 08:33:57 -0700

    sdk work

--- sdk/sdk_test.go
+++ sdk/sdk_test.go
@@ -1,1 +1,79 @@
 package sdk
+
+import (
+	_ "embed"
+	"github.com/ddkwork/librarygo/src/mycheck"
+	"github.com/ddkwork/librarygo/src/stream"
+	"github.com/ddkwork/librarygo/src/stream/tool"
+	"go/format"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestConstants(t *testing.T) { //Constants.h define only
+	path := "D:\\codespace\\workspace\\src\\cppkit\\gui\\sdk\\HyperDbgDev\\hyperdbg\\include\\SDK\\Headers\\Constants.h"
+	Constants, err := os.ReadFile(path)
+	if !mycheck.Error(err) {
+		return
+	}
+	lines, ok := tool.File().ToLines(Constants)
+	if !ok {
+		return
+	}
+	ss := make([]string, 0)
+	for i, line := range lines {
+		if strings.Contains(line, "SCRIPT_ENGINE_KERNEL_MODE") {
+			ss = lines[i+1:]
+			break
+		}
+	}
+
+	defines := make([]string, 0)
+	define := ""
+	for i := 0; i < len(ss); i++ {
+		switch {
+		case strings.Contains(ss[i], "#define") && strings.Contains(ss[i], `\`):
+			j := 0
+			define = ss[i+j]
+			for {
+				j++
+				define += ss[i+j]
+				define = strings.ReplaceAll(define, `\`, ``)
+				define = strings.Trim(define, " ")
+				if define != "" {
+					defines = append(defines, define)
+				}
+				if strings.Contains(ss[i+j], "#define") {
+					i += j
+					break
+				}
+			}
+		case strings.Contains(ss[i], "#define") && !strings.Contains(ss[i], `\`):
+			define = ss[i]
+			if define != "" {
+				defines = append(defines, define)
+			}
+		}
+	}
+
+	b := stream.New()
+	b.WriteStringLn("const(")
+	for _, define := range defines {
+		//println(define)
+		all := strings.ReplaceAll(define, "#define", "")
+		all = strings.TrimSpace(all)
+		index := strings.Index(all, " ")
+		key := all[:index]
+		value := all[index:]
+		key += "  =" + value
+		b.WriteStringLn(key)
+	}
+	b.WriteStringLn(")")
+	println(b.String())
+	source, err := format.Source(b.Bytes())
+	if !mycheck.Error(err) {
+		return
+	}
+	println(string(source))
+}
