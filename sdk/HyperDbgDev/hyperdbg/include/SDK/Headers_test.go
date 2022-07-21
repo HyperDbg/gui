73dff666e69fe3c0fa0024bde29ef677fb049b7c
Admin <2762713521@qq.com>
2022-07-21 11:51:03 -0700

    sdk work

---
+++ sdk/HyperDbgDev/hyperdbg/include/SDK/Headers_test.go
@@ -0,0 +1,35 @@
+package SDK
+
+import (
+	_ "embed"
+	"github.com/ddkwork/librarygo/src/mycheck"
+	"github.com/ddkwork/librarygo/src/stream"
+	"github.com/ddkwork/librarygo/src/windef"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+	paths := []string{
+		"./Headers/BasicTypes.h",
+		"./Headers./Constants.h",
+		"./Headers/ErrorCodes.h",
+		"./Headers/Connection.h",
+		"./Headers/Datatypes.h",
+		"./Headers/Ioctls.h",
+		"./Headers/Events.h",
+		"./Headers/RequestStructures.h",
+		"./Headers/Symbols.h",
+	}
+	buffer := stream.New()
+	for _, path := range paths {
+		b, err := os.ReadFile(path)
+		if !mycheck.Error(err) {
+			return
+		}
+		buffer.WriteBytesLn(b)
+	}
+	all := strings.ReplaceAll(buffer.String(), "#pragma once", "")
+	mycheck.Assert(t).True(windef.Creat(all, true))
+}
