781a4d0277912d15162f5a6625a5c2b0b0cb47a4
Admin <2762713521@qq.com>
2022-07-28 11:09:21 -0800

    add debugger.go

--- sdk/go/HyperDbgDev/hyperdbg/hprdbgctrl/code/debugger/core/debugger.go
+++ sdk/go/HyperDbgDev/hyperdbg/hprdbgctrl/code/debugger/core/debugger.go
@@ -9,9 +9,86 @@ var debuggerBuf string
 
 type (
 	Debugger interface {
-		//Fn() (ok bool)
+		ShowErrorMessage() (ok bool)
+		DebuggerGetNtoskrnlBase() (ok bool)
+		DebuggerPauseDebuggee() (ok bool)
+		IsConnectedToAnyInstanceOfDebuggerOrDebuggee() (ok bool)
+		IsTagExist() (ok bool)
+		InterpretScript() (ok bool)
+		InterpretConditionsAndCodes() (ok bool)
+		InterpretOutput() (ok bool)
+		SendEventToKernel() (ok bool)
+		RegisterActionToEvent() (ok bool)
+		GetNewDebuggerEventTag() (ok bool)
+		FreeEventsAndActionsMemory() (ok bool)
+		InterpretGeneralEventAndActionsFields() (ok bool)
 	}
 	debugger struct{}
 )
+
+func (d *debugger) ShowErrorMessage() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) DebuggerGetNtoskrnlBase() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) DebuggerPauseDebuggee() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) IsConnectedToAnyInstanceOfDebuggerOrDebuggee() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) IsTagExist() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) InterpretScript() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) InterpretConditionsAndCodes() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) InterpretOutput() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) SendEventToKernel() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) RegisterActionToEvent() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) GetNewDebuggerEventTag() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) FreeEventsAndActionsMemory() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *debugger) InterpretGeneralEventAndActionsFields() (ok bool) {
+	//TODO implement me
+	panic("implement me")
+}
 
 func Newdebugger() Debugger { return &debugger{} }
