093ad4d059c60c6ec13b3b50988e56317f281cd1
Admin <2762713521@qq.com>
2022-07-30 23:33:40 -0800

    unit test

--- sdk/cpp2go/cpp2go_test.go
+++ sdk/cpp2go/cpp2go_test.go
@@ -2,13 +2,23 @@ package cpp2go_test
 
 import (
 	"github.com/ddkwork/hyperdbgui/sdk/cpp2go"
-	"github.com/ddkwork/librarygo/src/myc2go"
 	"github.com/ddkwork/librarygo/src/mycheck"
 	"path/filepath"
 	"strings"
 	"testing"
 	"unsafe"
 )
+
+func TestCpp2Go(t *testing.T) {
+	o := cpp2go.NewObj()
+	o.Src("../HyperDbgDev/hyperdbg").
+		Dst("back").
+		ExpandPath("miscellaneous\\constants", ".txt").
+		Dst("back").
+		Back().
+		Convert()
+	//o.Format()
+}
 
 func TestUnsafe(t *testing.T) {
 	sizeofUINT32 := unsafe.Sizeof(uint32(0))
@@ -30,42 +40,4 @@ func Test2(t *testing.T) {
 	join := filepath.Join("back", after)
 	println(join)
 
-}
-
-func TestName(t *testing.T) {
-	o := cpp2go.NewObj()
-	o.Src("../HyperDbgDev/hyperdbg").
-		Dst("back").
-		ExpandPath("miscellaneous\\constants", ".txt").
-		Dst("back").
-		Back().
-		Convert()
-	//o.Format()
-}
-
-func TestC2go(t *testing.T) { //todo rewrite lib pkg
-	setup := myc2go.NewSetup(myc2go.Setup{
-		SetRoot: func() []string {
-			return []string{
-				//"./HyperDbgDev/hyperdbg/include/SDK/Headers",
-				"./HyperDbgDev",
-			}
-		},
-		SetExt: func() []string { return []string{".h", ".cpp", ".asm", ".txt"} },
-		SetContains: func() []string {
-			return []string{"hyperdbg\\miscellaneous",
-				"hyperdbg\\include",
-				"hyperdbg\\hprdbgctrl"}
-		},
-		SetContainsNot: func() []string { return []string{"build"} },
-		SetSkip:        func() string { return "SCRIPT_ENGINE_KERNEL_MODE" },
-		SetReplaces: func() map[string]string {
-			return map[string]string{
-				"sizeof(UINT32)":                 "4",
-				"sizeof(DEBUGGER_REMOTE_PACKET)": "11",
-			}
-		},
-		SetBasicTypes: func() string { return `Headers` },
-	})
-	mycheck.Assert(t).True(setup.ConvertAll())
 }
