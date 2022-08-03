fd7c674b2c35d863fff7aab90611e22d430d6ea1
Admin <2762713521@qq.com>
2022-08-03 09:34:37 -0800

    add debugger.go

--- hyperdbgui_test.go
+++
@@ -1,9 +0,0 @@
-package hyperdbgui
-
-//go:generate  go get github.com/ddkwork/librarygo@master
-//go:generate  go clean -modcache
-//go:generate  go work init
-//go:generate  go work use -r .
-//go:generate  go work sync
-//go:generate  go env -w GOPROXY=https://goproxy.cn
-//go:generate  go env -w GOPRIVATE=gitee.com
