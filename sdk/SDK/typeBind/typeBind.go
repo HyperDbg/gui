441ecb870707cfacb3645749c8e5649e9b8f9c18
u0_a711 <2762713521@qq.com>
2022-11-18 03:39:59 +0800

    sdk

---
+++ sdk/SDK/typeBind/typeBind.go
@@ -0,0 +1,28 @@
+package typeBind
+
+//sdk bind setup
+//c type to go type
+//heard file convert
+//enum const etc. to go
+//struct to go
+//all methods to go
+//warp all methods into interface
+//unit test: call dll by sdk
+//
+//
+//
+//
+//
+
+// replace all codes by this
+type (
+	BOOLEAN bool
+	UINT64  uint64
+	UINT32  uint32
+	wchar_t rune
+
+	VOID uintptr
+	//todo test, if it is a adress
+	//the value will to be a uint64 value
+
+)
