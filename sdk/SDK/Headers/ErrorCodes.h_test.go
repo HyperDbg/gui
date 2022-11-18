3be12b16ce1de74042cb69ef777e674f51763a40
u0_a711 <2762713521@qq.com>
2022-11-18 13:56:38 +0800

    sdk

--- sdk/SDK/Headers/ErrorCodes.h_test.go
+++ sdk/SDK/Headers/ErrorCodes.h_test.go
@@ -3,6 +3,8 @@ package Headers
 import (
 	"github.com/ddkwork/golibrary/mylog"
 	"github.com/ddkwork/golibrary/src/stream"
+	"github.com/ddkwork/golibrary/src/stream/tool"
+	"go/format"
 	"strings"
 	"sync"
 	"testing"
@@ -13,7 +15,13 @@ func TestGenErrorCodes(t *testing.T) {
 	body.WriteStringLn("package Headers")
 	body.WriteStringLn("type ErrorCodes int")
 	body.WriteStringLn("const (")
-	lines, ok := stream.New().ReadToLines("ErrorCodes.h")
+	file := stream.NewReadFile("ErrorCodes.h")
+	all := strings.ReplaceAll(file.String(), `\
+   `, "")
+	file.Reset()
+	file.WriteString(all)
+	lines, ok := file.ToLines(file.Bytes())
+	//lines, ok := stream.New().ReadToLines("ErrorCodes.h")
 	if !ok {
 		return
 	}
@@ -33,5 +41,22 @@ func TestGenErrorCodes(t *testing.T) {
 		}
 	}
 	body.WriteStringLn(")")
+	body.WriteStringLn("func (e ErrorCodes)String()string{")
+
+	body.WriteStringLn("}")
 	mylog.Json("gen error code", body.String())
+	source, err := format.Source(body.Bytes())
+	if !mylog.Error(err) {
+		tool.File().WriteTruncate("ErrorCodes.h.go", body.Bytes())
+		return
+	}
+	tool.File().WriteTruncate("ErrorCodes.h.go", source)
+}
+
+func TestRemoveNewline(t *testing.T) {
+	code := `#define DEBUGGER_ERROR_EDIT_MEMORY_STATUS_INVALID_ADDRESS_BASED_ON_OTHER_PROCESS \
+    0xc000000d`
+	all := strings.ReplaceAll(code, `\
+   `, "")
+	println(all)
 }
