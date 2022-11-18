60615d6e5445da7e23f1f6d70f056b2c88a35906
u0_a711 <2762713521@qq.com>
2022-11-18 09:55:21 +0800

    sdk

---
+++ sdk/SDK/Headers/BasicTypes.h.go
@@ -0,0 +1,13 @@
+package Headers
+
+type (
+	BOOLEAN bool
+	UINT64  uint64
+	UINT32  uint32
+	wchar_t rune
+
+	VOID uintptr
+	//todo test, if it is a adress
+	//the value will to be a uint64 value
+
+)
