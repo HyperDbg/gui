330eb262f17342e62f7e19bbb7e045a4fe03fbd8
ddkwork <2762713521@qq.com>
2024-01-27 22:44:32 -0800

    reset lock

--- HyperDbg.go
+++ HyperDbg.go
@@ -23,32 +23,54 @@ import ( //
 //go:generate core build -v -t android/arm64
 //go:generate core build -v -t windows/amd64
 
-//go:embed SND/ICO/*.ico
-var myIcons embed.FS
-
 //go:embed SND/ICO/ICO_AAAMAIN.svg
 var mainIcons []byte
+
+//go:embed SND/ICO/*.svg
+var myIcons embed.FS
+
+//go:embed SND/png/*.svg
+var mypngs embed.FS
 
 //go:embed SND/pageIco/*.svg
 var pageIco embed.FS
 
 func main() {
 	icons.AddFS(grr.Log1(fs.Sub(myIcons, "SND/ICO")))
+	icons.AddFS(grr.Log1(fs.Sub(myIcons, "SND/png")))
 	icons.AddFS(grr.Log1(fs.Sub(pageIco, "SND/pageIco")))
 	gi.TheApp.SetIconBytes(mainIcons)
 	b := gi.NewBody("HyperDbg")
 	b.AddAppBar(func(tb *gi.Toolbar) {
-		widget.NewButton(tb).SetTooltip("open").SetIcon("1")
-		widget.NewButton(tb).SetTooltip("reload").SetIcon("2")
-		widget.NewButton(tb).SetTooltip("stop").SetIcon("3")
-		widget.NewButton(tb).SetTooltip("f7").SetIcon("4")
-		widget.NewButton(tb).SetTooltip("f8").SetIcon("5")
-		widget.NewButton(tb).SetTooltip("f9").SetIcon("6")
-		widget.NewButton(tb).SetTooltip("scylla").SetIcon("7")
-		widget.NewButton(tb).SetTooltip("path").SetIcon("8")
-		widget.NewButton(tb).SetTooltip("calc").SetIcon("9")
-		widget.NewButton(tb).SetTooltip("setting").SetIcon("10")
-		widget.NewButton(tb).SetTooltip("about").SetIcon("11")
+		widget.NewButton(tb).SetTooltip("open").SetIcon("OPEN")
+		widget.NewButton(tb).SetTooltip("RESTART").SetIcon("RESTART")
+		widget.NewButton(tb).SetTooltip("CLOSE").SetIcon("CLOSE")
+		widget.NewButton(tb).SetTooltip("RUN").SetIcon("RUN") //.SetShortcut()//todo
+		widget.NewButton(tb).SetTooltip("RUNTHREAD").SetIcon("RUNTHREAD")
+		widget.NewButton(tb).SetTooltip("PAUSE").SetIcon("PAUSE")
+		gi.NewSeparator(tb)
+		widget.NewButton(tb).SetTooltip("STEPIN").SetIcon("STEPIN")
+		widget.NewButton(tb).SetTooltip("STEPOVER").SetIcon("STEPOVER")
+		widget.NewButton(tb).SetTooltip("STEPIN").SetIcon("STEPIN")
+		widget.NewButton(tb).SetTooltip("TRIN").SetIcon("TRIN")
+		widget.NewButton(tb).SetTooltip("TROVER").SetIcon("TROVER")
+		widget.NewButton(tb).SetTooltip("TILLRET").SetIcon("TILLRET")
+		widget.NewButton(tb).SetTooltip("TILLUSER").SetIcon("TILLUSER")
+		gi.NewSeparator(tb)
+		widget.NewButton(tb).SetTooltip("LOG").SetIcon("LOG")
+		widget.NewButton(tb).SetTooltip("MODULES").SetIcon("MODULES")
+		widget.NewButton(tb).SetTooltip("WINDOWS").SetIcon("WINDOWS")
+		widget.NewButton(tb).SetTooltip("THREADS").SetIcon("THREADS")
+		widget.NewButton(tb).SetTooltip("CPU").SetIcon("CPU")
+		widget.NewButton(tb).SetTooltip("SEARCH").SetIcon("SEARCH")
+		widget.NewButton(tb).SetTooltip("TRACE").SetIcon("TRACE")
+		gi.NewSeparator(tb)
+		widget.NewButton(tb).SetTooltip("BPOINTS").SetIcon("BPOINTS")
+		widget.NewButton(tb).SetTooltip("BPMEM").SetIcon("BPMEM")
+		widget.NewButton(tb).SetTooltip("BPHARD").SetIcon("BPHARD")
+		widget.NewButton(tb).SetTooltip("OPTIONS").SetIcon("OPTIONS")
+		widget.NewButton(tb).SetTooltip("scylla").SetIcon("scylla")
+		widget.NewButton(tb).SetTooltip("about").SetIcon("about")
 	})
 
 	tabs := gi.NewTabs(b)
