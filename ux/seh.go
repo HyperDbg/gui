74df6630e9fb72875e496d7a39d6690e22c316e2
ddkwork <2762713521@qq.com>
2024-06-13 22:09:03 +0800

    remove core

--- ux/seh.go
+++ ux/seh.go
@@ -1,13 +1,39 @@
 package ux
 
 import (
-	"cogentcore.org/core/gi"
-	"cogentcore.org/core/giv"
+	"fmt"
+	"github.com/ddkwork/app/widget"
 	"github.com/richardwilkes/unison"
 )
 
 func LayoutSeh(parent unison.Paneler) unison.Paneler {
-	SehTable(parent)
+	table, header := widget.NewTable(Seh{}, widget.TableContext[Seh]{
+		ContextMenuItems: nil,
+		MarshalRow: func(node *widget.Node[Seh]) (cells []widget.CellData) {
+			return []widget.CellData{
+				{Text: fmt.Sprintf("%016X", node.Data.Address)},
+				{Text: node.Data.ExceptionHandlingRoutines},
+				{Text: node.Data.Label},
+				{Text: node.Data.Notes},
+			}
+		},
+		UnmarshalRow:             nil,
+		SelectionChangedCallback: nil,
+		SetRootRowsCallBack: func(root *widget.Node[Seh]) {
+			for i := range 100 {
+				ts := Seh{
+					Address:                   0x00000002429BF7B8 + i,
+					ExceptionHandlingRoutines: "xxx",
+					Label:                     "www",
+					Notes:                     "this is notes",
+				}
+				root.AddChildByData(ts)
+			}
+		},
+		JsonName:   "",
+		IsDocument: false,
+	})
+	return widget.NewTableScrollPanel(parent, table, header)
 }
 
 type Seh struct {
@@ -15,21 +41,4 @@ type Seh struct {
 	ExceptionHandlingRoutines string
 	Label                     string
 	Notes                     string
-}
-
-func SehTable(frame *gi.Frame) *giv.TableView {
-	breaks := make([]*Seh, 100)
-	for i := range breaks {
-		ts := &Seh{
-			Address:                   0x00000002429BF7B8 + i,
-			ExceptionHandlingRoutines: "xxx",
-			Label:                     "www",
-			Notes:                     "this is notes",
-		}
-		breaks[i] = ts
-	}
-	tv := giv.NewTableView(frame, "tv")
-	tv.SetReadOnly(true)
-	tv.SetSlice(&breaks)
-	return tv
 }
