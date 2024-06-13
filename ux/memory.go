aa5ba53ca1887c1895441d30e1784185396ce13a
ddkwork <2762713521@qq.com>
2024-06-13 22:11:40 +0800

    remove core

--- ux/memory.go
+++ ux/memory.go
@@ -1,7 +1,8 @@
 package ux
 
 import (
-	"cogentcore.org/core/giv"
+	"fmt"
+	"github.com/ddkwork/app/widget"
 	"github.com/richardwilkes/unison"
 )
 
@@ -16,21 +17,38 @@ type Memory struct {
 }
 
 func LayoutMemory(parent unison.Paneler) unison.Paneler {
-	breaks := make([]*Memory, 100)
-	for i := range breaks {
-		ts := &Memory{
-			Address:           0x000001027D020000 + i,
-			Size:              0o000000000011000,
-			Level:             "用户模块",
-			PageInfo:          "\\Device\\HarddiskVolume3\\Windows\\System32\\C_1252.NLS",
-			AssignmentType:    "MAP",
-			PageProtection:    "-R---",
-			InitialProtection: "-R---",
-		}
-		breaks[i] = ts
-	}
-	tv := giv.NewTableView(frame, "tv")
-	tv.SetReadOnly(true)
-	tv.SetSlice(&breaks)
-	return tv
+
+	table, header := widget.NewTable(Memory{}, widget.TableContext[Memory]{
+		ContextMenuItems: nil,
+		MarshalRow: func(node *widget.Node[Memory]) (cells []widget.CellData) {
+			return []widget.CellData{
+				{Text: fmt.Sprintf("%d", node.Data.Address)},
+				{Text: fmt.Sprintf("%d", node.Data.Size)},
+				{Text: node.Data.Level},
+				{Text: node.Data.PageInfo},
+				{Text: node.Data.AssignmentType},
+				{Text: node.Data.PageProtection},
+				{Text: node.Data.InitialProtection},
+			}
+		},
+		UnmarshalRow:             nil,
+		SelectionChangedCallback: nil,
+		SetRootRowsCallBack: func(root *widget.Node[Memory]) {
+			for i := range 100 {
+				ts := Memory{
+					Address:           0x000001027D020000 + i,
+					Size:              0o000000000011000,
+					Level:             "用户模块",
+					PageInfo:          "\\Device\\HarddiskVolume3\\Windows\\System32\\C_1252.NLS",
+					AssignmentType:    "MAP",
+					PageProtection:    "-R---",
+					InitialProtection: "-R---",
+				}
+				root.AddChildByData(ts)
+			}
+		},
+		JsonName:   "",
+		IsDocument: false,
+	})
+	return widget.NewTableScrollPanel(parent, table, header)
 }
--- ux/script.go
+++ ux/script.go
@@ -1,33 +1,41 @@
 package ux
 
 import (
-	"cogentcore.org/core/gi"
-	"cogentcore.org/core/giv"
+	"fmt"
+	"github.com/ddkwork/app/widget"
 	"github.com/richardwilkes/unison"
 )
 
 func LayoutScript(parent unison.Paneler) unison.Paneler {
-	ScriptTable(parent)
+	table, header := widget.NewTable(Script{}, widget.TableContext[Script]{
+		ContextMenuItems: nil,
+		MarshalRow: func(node *widget.Node[Script]) (cells []widget.CellData) {
+			return []widget.CellData{
+				{Text: fmt.Sprintf("%d", node.Data.Line)},
+				{Text: node.Data.Contest},
+				{Text: node.Data.Info},
+			}
+		},
+		UnmarshalRow:             nil,
+		SelectionChangedCallback: nil,
+		SetRootRowsCallBack: func(root *widget.Node[Script]) {
+			for i := range 100 {
+				ts := Script{
+					Line:    i,
+					Contest: "run",
+					Info:    "run code",
+				}
+				root.AddChildByData(ts)
+			}
+		},
+		JsonName:   "",
+		IsDocument: false,
+	})
+	return widget.NewTableScrollPanel(parent, table, header)
 }
 
 type Script struct {
 	Line    int
 	Contest string
 	Info    string
-}
-
-func ScriptTable(frame *gi.Frame) *giv.TableView {
-	breaks := make([]*Script, 100)
-	for i := range breaks {
-		ts := &Script{
-			Line:    i,
-			Contest: "run",
-			Info:    "run code",
-		}
-		breaks[i] = ts
-	}
-	tv := giv.NewTableView(frame, "tv")
-	tv.SetReadOnly(true)
-	tv.SetSlice(&breaks)
-	return tv
 }
