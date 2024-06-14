1bc99ef242832750e7168d792f55a75851ce09b7
ddkwork <2762713521@qq.com>
2024-06-14 23:54:03 +0800

    implementation pe treeTable

--- ux/peView.go
+++ ux/peView.go
@@ -9,9 +9,9 @@ import (
 
 type PeView struct {
 	Name    string
-	Offset  uint64
-	Address uint64
-	Size    uint64
+	Offset  uint32
+	Address uint32
+	Size    uint32
 	Is64Bit bool
 	//overlay *pe.Overlay todo
 }
@@ -21,10 +21,11 @@ func layoutPeView(name string, parent unison.Paneler) unison.Paneler {
 		ContextMenuItems: nil,
 		MarshalRow: func(node *widget.Node[PeView]) (cells []widget.CellData) {
 			return []widget.CellData{
+				{Text: node.Data.Name},
+				{Text: fmt.Sprintf("%08X", node.Data.Offset)},
+				{Text: fmt.Sprintf("%08X", node.Data.Size)},
 				{Text: fmt.Sprintf("%016X", node.Data.Address)},
-				{Text: node.Data.ExceptionHandlingRoutines},
-				{Text: node.Data.Label},
-				{Text: node.Data.Notes},
+				{Text: fmt.Sprintf("%t", node.Data.Is64Bit)},
 			}
 		},
 		UnmarshalRow:             nil,
@@ -35,21 +36,42 @@ func layoutPeView(name string, parent unison.Paneler) unison.Paneler {
 				Import  = "Import"
 				Export  = "Export"
 			)
+			if name == "" {
+				return
+			}
 			pe := xed.ParserPe(name)
 			containerNodes := widget.NewContainerNodes[PeView]([]string{Section, Import, Export})
 			for _, node := range containerNodes {
 				switch node.Type {
 				case Section:
-					for i, section := range pe.Sections {
-						node.AddChildByData(PeView{})
+					for _, section := range pe.Sections {
+						node.AddChildByData(PeView{
+							Name:    section.String(),
+							Offset:  section.Header.PointerToRawData,
+							Address: section.Header.VirtualAddress,
+							Size:    section.Header.VirtualSize,
+							Is64Bit: pe.Is64,
+						})
 					}
 				case Import:
-					for i, i2 := range pe.Imports {
-						node.AddChildByData(PeView{})
+					for _, v := range pe.Imports {
+						node.AddChildByData(PeView{
+							Name:   v.Name,
+							Offset: v.Offset,
+							//Address: v.,
+							Size:    0,
+							Is64Bit: pe.Is64,
+						})
 					}
 				case Export:
-					for i, function := range pe.Export.Functions {
-						node.AddChildByData(PeView{})
+					for _, function := range pe.Export.Functions {
+						node.AddChildByData(PeView{
+							Name:    function.Name,
+							Offset:  function.FunctionRVA,
+							Address: 0,
+							Size:    0,
+							Is64Bit: pe.Is64,
+						})
 					}
 				}
 				root.AddChild(node)
