725fbc2e010267cca24cd7cdeb68c1a80ae1498d
ddkwork <2762713521@qq.com>
2024-06-16 14:16:23 +0800

    start Control

--- upLib.cmd
+++ upLib.cmd
@@ -1,2 +1,2 @@
-go get github.com/ddkwork/app@904521728d42170eef1e2e87111244c31a1aa66f
+go get github.com/ddkwork/app@8cfc4bd12e9c79d45d436229792a7fbc84a34d8f
 go get github.com/ddkwork/golibrary@ce26f19ff2e5888064e5b1dd0ee6537e69da6c7a
