d79b87cb3e136f0cc343f2fc4496f4be66395f06
ddkwork <2762713521@qq.com>
2024-06-18 01:02:02 +0800

    generated function

--- control/tmp/hprdbgctrl/code/common/common.go
+++ control/tmp/hprdbgctrl/code/common/common.go
@@ -1,45 +1,45 @@
 package control
 
-func SeparateTo64BitValue() { // hprdbgctrl\code\common\common.cpp:4
+func SeparateTo64BitValue() string { // hprdbgctrl\code\common\common.cpp:4
 }
 
 func PrintBits() { // hprdbgctrl\code\common\common.cpp:12
 }
 
-func Replace() { // hprdbgctrl\code\common\common.cpp:24
+func Replace() bool { // hprdbgctrl\code\common\common.cpp:24
 }
 
 func ReplaceAll() { // hprdbgctrl\code\common\common.cpp:31
 }
 
-func Split() { // hprdbgctrl\code\common\common.cpp:40
+func Split() string { // hprdbgctrl\code\common\common.cpp:40
 }
 
-func IsNumber() { // hprdbgctrl\code\common\common.cpp:55
+func IsNumber() bool { // hprdbgctrl\code\common\common.cpp:55
 }
 
-func IsHexNotation() { // hprdbgctrl\code\common\common.cpp:59
+func IsHexNotation() bool { // hprdbgctrl\code\common\common.cpp:59
 }
 
-func IsDecimalNotation() { // hprdbgctrl\code\common\common.cpp:72
+func IsDecimalNotation() bool { // hprdbgctrl\code\common\common.cpp:72
 }
 
-func HexToBytes() { // hprdbgctrl\code\common\common.cpp:85
+func HexToBytes() byte { // hprdbgctrl\code\common\common.cpp:85
 }
 
-func ConvertStringToUInt64() { // hprdbgctrl\code\common\common.cpp:94
+func ConvertStringToUInt64() bool { // hprdbgctrl\code\common\common.cpp:94
 }
 
-func ConvertStringToUInt32() { // hprdbgctrl\code\common\common.cpp:153
+func ConvertStringToUInt32() bool { // hprdbgctrl\code\common\common.cpp:153
 }
 
-func HasEnding() { // hprdbgctrl\code\common\common.cpp:208
+func HasEnding() bool { // hprdbgctrl\code\common\common.cpp:208
 }
 
-func ValidateIP() { // hprdbgctrl\code\common\common.cpp:218
+func ValidateIP() bool { // hprdbgctrl\code\common\common.cpp:218
 }
 
-func HyperDbgVmxSupportDetection() { // hprdbgctrl\code\common\common.cpp:228
+func HyperDbgVmxSupportDetection() bool { // hprdbgctrl\code\common\common.cpp:228
 }
 
 func ltrim() { // hprdbgctrl\code\common\common.cpp:260
@@ -54,19 +54,19 @@ func Trim() { // hprdbgctrl\code\common\common.cpp:268
 func RemoveSpaces() { // hprdbgctrl\code\common\common.cpp:272
 }
 
-func IsFileExistA() { // hprdbgctrl\code\common\common.cpp:276
+func IsFileExistA() bool { // hprdbgctrl\code\common\common.cpp:276
 }
 
-func IsFileExistW() { // hprdbgctrl\code\common\common.cpp:280
+func IsFileExistW() bool { // hprdbgctrl\code\common\common.cpp:280
 }
 
-func IsEmptyString() { // hprdbgctrl\code\common\common.cpp:284
+func IsEmptyString() bool { // hprdbgctrl\code\common\common.cpp:284
 }
 
 func GetConfigFilePath() { // hprdbgctrl\code\common\common.cpp:297
 }
 
-func ListDirectory() { // hprdbgctrl\code\common\common.cpp:303
+func ListDirectory() string { // hprdbgctrl\code\common\common.cpp:303
 }
 
 func StringToWString() { // hprdbgctrl\code\common\common.cpp:317
@@ -75,29 +75,29 @@ func StringToWString() { // hprdbgctrl\code\common\common.cpp:317
 func SplitPathAndArgs() { // hprdbgctrl\code\common\common.cpp:321
 }
 
-func FindCaseInsensitive() { // hprdbgctrl\code\common\common.cpp:362
+func FindCaseInsensitive() int { // hprdbgctrl\code\common\common.cpp:362
 }
 
-func FindCaseInsensitiveW() { // hprdbgctrl\code\common\common.cpp:367
+func FindCaseInsensitiveW() int { // hprdbgctrl\code\common\common.cpp:367
 }
 
-func ConvertStringVectorToCharPointerArray() { // hprdbgctrl\code\common\common.cpp:372
+func ConvertStringVectorToCharPointerArray() string { // hprdbgctrl\code\common\common.cpp:372
 }
 
 func CommonCpuidInstruction() { // hprdbgctrl\code\common\common.cpp:377
 }
 
-func Getx86VirtualAddressWidth() { // hprdbgctrl\code\common\common.cpp:380
+func Getx86VirtualAddressWidth() uint32 { // hprdbgctrl\code\common\common.cpp:380
 }
 
-func CheckCpuSupportRtm() { // hprdbgctrl\code\common\common.cpp:385
+func CheckCpuSupportRtm() bool { // hprdbgctrl\code\common\common.cpp:385
 }
 
-func CheckAddressCanonicality() { // hprdbgctrl\code\common\common.cpp:394
+func CheckAddressCanonicality() bool { // hprdbgctrl\code\common\common.cpp:394
 }
 
-func CheckAddressValidityUsingTsx() { // hprdbgctrl\code\common\common.cpp:412
+func CheckAddressValidityUsingTsx() bool { // hprdbgctrl\code\common\common.cpp:412
 }
 
-func CheckAccessValidityAndSafety() { // hprdbgctrl\code\common\common.cpp:426
+func CheckAccessValidityAndSafety() bool { // hprdbgctrl\code\common\common.cpp:426
 }
