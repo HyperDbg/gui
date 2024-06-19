c4cde4c25c69d3c3cd68e62593ed14f1d770f384
Admin <2762713521@qq.com>
2024-06-19 10:21:07 +0800

    bind remove old implementation

---
+++ old_delete/test/script/s.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/ddkwork/app"
+	"github.com/ddkwork/hyperdbgui/ux"
+	"github.com/richardwilkes/unison"
+)
+
+func main() {
+	app.Run("script", func(w *unison.Window) {
+		ux.LayoutScript(w.Content())
+	})
+}
