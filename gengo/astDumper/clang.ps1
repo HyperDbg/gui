3d17d8bffeeac62383a76dbec64dfc97b3fcc604
ddkwork <2762713521@qq.com>
2024-06-22 13:37:46 +0800

    TestBindMacros

--- gengo/astDumper/clang.ps1
+++
@@ -1,4 +0,0 @@
-clang++ -nobuiltininc -Xclang -ast-dump=json -fsyntax-only test.hpp  > ast_output.json 2>&1
-clang++ -fsyntax-only -nobuiltininc -emit-llvm -Xclang -fdump-record-layouts -Xclang -fdump-record-layouts-complete test.hpp  > ast_record_layouts.log 2>&1
-clang++ -E -dM test.hpp > macros.txt
-pause
