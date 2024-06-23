d3e00843414a5db9772d1cf4b7416bc6cd61af03
Admin <2762713521@qq.com>
2024-06-23 13:02:35 +0800

    TestSdk

--- ux/bin/debug/HPRDBGCTRL_test.go
+++ ux/bin/debug/HPRDBGCTRL_test.go
@@ -2,30 +2,36 @@ package HPRDBGCTRL
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/ddkwork/golibrary/mylog"
 	"github.com/ddkwork/golibrary/stream"
-
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSdk(t *testing.T) {
-	stream.CopyFile("hprdbgkd.sys", filepath.Join("C:\\Windows\\System32\\drivers\\hprdbgkd.sys"))
+	sysName := "hprdbgkd.sys"
+	path := filepath.Join("C:\\Windows\\System32\\drivers", sysName)
+	mylog.Call(func() {
+		mylog.Check(os.Remove(path))
+		stream.CopyFile(sysName, path)
 
-	assert.True(t, HyperDbgVmxSupportDetection()) // ok
-	s := make([]byte, 0, 100)
-	HyperDbgReadVendorString(s) // todo bug
-	fmt.Println(s)
+		assert.True(t, HyperDbgVmxSupportDetection()) // ok
+		s := make([]byte, 0, 100)
+		HyperDbgReadVendorString(s) // todo bug
+		fmt.Println(s)
 
-	HyperDbgInstallVmmDriver()
-	// HyperDbgStartVmmDriver()
+		HyperDbgInstallVmmDriver()
+		// HyperDbgStartVmmDriver()
 
-	// missing connection local or start ?
-	HyperDbgLoadVmm()
+		// missing connection local or start ?
+		HyperDbgLoadVmm()
 
-	// unloading vmm driver before need wait 1 second?
-	// HyperDbgStopVmmDriver()
-	HyperDbgUnloadVmm()
-	HyperDbgUninstallVmmDriver()
+		// unloading vmm driver before need wait 1 second?
+		// HyperDbgStopVmmDriver()
+		HyperDbgUnloadVmm()
+		HyperDbgUninstallVmmDriver()
+	})
 }
