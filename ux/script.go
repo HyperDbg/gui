906488f4f2e01626742c8618d78f028be31ca4fb
ddkwork <2762713521@qq.com>
2024-06-24 03:41:51 +0800

    fix script layout

--- ux/script.go
+++ ux/script.go
@@ -7,5 +7,6 @@ import (
 
 func LayoutScript(parent unison.Paneler) unison.Paneler {
 	path := "ux/send_version.scala"
+	// path = "ux/plugin/scriptGen/cpuid.ds"
 	return widget.NewCodeEditor(parent, path)
 }
