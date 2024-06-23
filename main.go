0a332d01d936b3d85d6040a40837f82be12e14f6
Admin <2762713521@qq.com>
2024-06-23 13:34:51 +0800

    add Release

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
-import "github.com/ddkwork/hyperdbgui/ux"
+import (
+	"github.com/ddkwork/golibrary/mylog"
+	"github.com/ddkwork/golibrary/stream"
+	"github.com/ddkwork/hyperdbgui/ux"
+	"os"
+	"path/filepath"
+)
 
 //go:generate go build .
 //go:generate go install .
 
 func main() {
 	ux.Run()
+}
+
+func Release() {
+	stream.RunCommand("go build .")
+	stream.CopyDir("hyperdbgui.exe", "bin/debug")
+	stream.RunCommand("git clone --recursive https://github.com/HyperDbg/HyperDbg.git")
+	stream.RunCommand("HyperDbg/utils/msvc-build.bat")
+	filepath.Walk("bin/debug", func(path string, info fs.FileInfo, err error) error {
+		switch filepath.Ext(path) {
+		case ".pdb", ".exp", ".lib":
+			mylog.Check(os.Remove(path))
+		}
+		return err
+	})
+	mylog.Todo("remove outer files or dirs")
+	stream.RunCommand("tar -zcvf hyperdbgui.tar.gz bin/debug")
 }
