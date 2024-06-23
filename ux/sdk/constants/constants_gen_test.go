1fb647e3fec239bca78a00cb9b55f6a86ae0c794
Admin <2762713521@qq.com>
2024-06-23 19:32:04 +0800

    genConstants

--- ux/sdk/constants/constants_gen_test.go
+++ ux/sdk/constants/constants_gen_test.go
@@ -14,28 +14,36 @@ import (
 
 func TestGenConstants(t *testing.T) {
 	mylog.Call(func() {
-		genConstants("errors.txt")
 		genConstants("ioctl.txt")
+		genConstants("errors.txt")
 		genConstants("ntstatus.txt")
 	})
 }
 
 func genConstants(fileName string) {
 	m := orderedmap.New[string, string]()
-	for _, s := range stream.NewBuffer(fileName).ToLines() {
+	for i, s := range stream.NewBuffer(fileName).ToLines() {
+		if i == 4 {
+			// break // test
+		}
 		split := strings.Split(s, " ")
-		m.Set(split[0], split[1])
+		v := split[1]
+		if fileName == "ioctl.txt" {
+			v = "0x" + v
+		}
+		m.Set(split[0], v)
 	}
 
-	kind := stream.BaseName(fileName) + "Kind"
+	kind := stream.ToCamelUpper(stream.BaseName(fileName), false) + "Kind"
 
 	g := stream.NewGeneratedFile()
 	g.P("package constants")
-	g.P("type ", kind, " byte")
+	g.P("type ", kind, " int")
 	g.P("const (")
 	for i, p := range m.List() {
 		if i == 0 {
-			g.P(p.Key, kind, " = ", p.Value)
+			g.P(p.Key, " ", kind, " = ", p.Value)
+			continue
 		}
 		g.P(p.Key, " = ", p.Value)
 	}
@@ -44,26 +52,22 @@ func genConstants(fileName string) {
 
 	g.P("func (k ", kind, ") String() string {")
 	g.P("switch k {")
-	for i, p := range m.List() {
-		if i == 0 {
-			continue
-		}
+	for _, p := range m.List() {
 		g.P("case ", p.Key, ":")
 		g.P("return ", strconv.Quote(p.Value)) // todo rename
 	}
 	g.P("default:")
 	g.P("return \"unknown\"")
 	g.P("}")
+	g.P("}")
 	g.P()
 
-	g.P("func (k ", kind, ") Elements() {")
+	g.P("func(k ", kind, ") Elements()[]", kind, " {")
 	g.P("return []", kind, " {")
-	for i, p := range m.List() {
-		if i == 0 {
-			continue
-		}
+	for _, p := range m.List() {
 		g.P(p.Key, ",")
 	}
+	g.P("}")
 	g.P("}")
 	g.P()
 
