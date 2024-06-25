e08a4d53164ba2030f119c7514085d12623ebeaf
ddkwork <2762713521@qq.com>
2024-06-25 14:57:24 +0800

    start ui events

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"github.com/ddkwork/app"
+	"github.com/richardwilkes/unison"
+)
+
 func main() {
 	Run()
+}
+func testDisassembly() {
+	app.Run("asm", func(w *unison.Window) {
+		LayoutDisassemblyTable("ux\\bin\\debug\\hyperdbg-cli.exe", w.Content())
+	})
+}
+
+func testParsePe() {
+	app.Run("pe", func(w *unison.Window) {
+		LayoutPeView("ux\\bin\\debug\\HPRDBGCTRL.dll", w.Content())
+	})
+}
+func testScript() {
+	app.Run("script", func(w *unison.Window) {
+		LayoutScript(w.Content())
+	})
 }
