377762d2403535fdc37d8f217cf0b8ea4700dca9
Admin <2762713521@qq.com>
2024-06-28 14:43:10 +0800

    update widgets

--- libhyperdbg_test.go
+++ libhyperdbg_test.go
@@ -33,6 +33,47 @@ func TestSdk(t *testing.T) {
 }
 
 /*
+=== RUN   TestSdk
+2024-06-28 14:42:27     Info ->                            │ virtualization technology is vt-x //github.com/ddkwork/hyperdbgui.VmxSupportDetection+0xbd D:/workspace/workspace/branch/gui/util.go:47
+2024-06-28 14:42:27   Struct ->                            │  //github.com/ddkwork/hyperdbgui.VmxSupportDetection+0xf8 D:/workspace/workspace/branch/gui/util.go:48
+{
+ "Cpu0": {
+  "Eax": 27,
+  "Ebx": 1970169159,
+  "Ecx": 1818588270,
+  "Edx": 1231384169
+ },
+ "Cpu1": {
+  "Eax": 526017,
+  "Ebx": 1050624,
+  "Ecx": 2147154879,
+  "Edx": 3219913727
+ },
+ "Vendor": "GenuineIntel",
+ "ProcessorBrandString": "11th Gen Intel(R) Core(TM) i7-1160G7 @ 1.20GHz\u0000\u0000"
+}
+2024-06-28 14:42:27     Info ->                            │ vmx operation is supported by your processor //github.com/ddkwork/hyperdbgui.VmxSupportDetection+0x135 D:/workspace/workspace/branch/gui/util.go:53
+2024-06-28 14:42:27    Trace ->           InstallVmmDriver │ 1 //github.com/ddkwork/hyperdbgui.TestSdk.func1+0x45 D:/workspace/workspace/branch/gui/libhyperdbg_test.go:15
+2024-06-28 14:42:27    Trace ->                    LoadVmm │ 1 //github.com/ddkwork/hyperdbgui.TestSdk.func1+0xb1 D:/workspace/workspace/branch/gui/libhyperdbg_test.go:17
+2024-06-28 14:42:27    Trace ->                  UnloadVmm │ 1 //github.com/ddkwork/hyperdbgui.TestSdk.func1+0x100 D:/workspace/workspace/branch/gui/libhyperdbg_test.go:29
+2024-06-28 14:42:27    Trace ->              StopVmmDriver │ 1 //github.com/ddkwork/hyperdbgui.TestSdk.func1+0x146 D:/workspace/workspace/branch/gui/libhyperdbg_test.go:30
+2024-06-28 14:42:27    Trace ->         UninstallVmmDriver │ 1 //github.com/ddkwork/hyperdbgui.TestSdk.func1+0x18c D:/workspace/workspace/branch/gui/libhyperdbg_test.go:31
+--- PASS: TestSdk (0.00s)
+PASS
+err, target file is not loaded
+current processor vendor is : GenuineIntel
+virtualization technology is vt-x
+vmx operation is supported by your processor
+err, CreateFile failed (2)
+handle of the driver not found, probably the driver is not loaded. Did you use 'load' command?
+err, OpenService failed (424)
+err, OpenService failed (424)
+
+Process finished with the exit code 0
+
+*/
+
+/*
 .connect local
 load vmm
 .sympath SRV*c:\Symbols*https://msdl.microsoft.com/download/symbol
