f85c41bfffe261b1b5c004eee64160ac588dc908
Admin <2762713521@qq.com>
2024-07-02 21:02:03 +0800

    layout vstart

--- main.go
+++ main.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"github.com/ddkwork/app"
+	"github.com/ddkwork/golibrary/mylog"
 	"github.com/richardwilkes/unison"
 )
 
 func main() {
+	mylog.Call(func() {
+		VmxSupportDetection()
+		mylog.Trace("InstallVmmDriver", InstallVmmDriver())
+		ConnectLocalDebugger()
+		mylog.Trace("LoadVmm", LoadVmm())
+
+		// todo:
+		// start debugger
+		// read memory(todo bug read address buffer for disassembly,see cpu.go LayoutDisassemblyTable ,oep buf seems error)
+		// read registers
+		// set breakpoints
+		// step over/into/out
+		// continue
+		// read stack
+		// stop debugger
+
+		mylog.Trace("UnloadVmm", UnloadVmm())
+		mylog.Trace("StopVmmDriver", StopVmmDriver())
+		mylog.Trace("UninstallVmmDriver", UninstallVmmDriver())
+	})
+
 	// testLayoutCpu()
-	// return
+	return
 	Run()
 	return
 	// testDisassembly()
