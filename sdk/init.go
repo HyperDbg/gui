64a26aaf44a7e76c890239b0fbd718bb1da464e8
Admin <2762713521@qq.com>
2024-07-03 23:06:04 +0800

    embed bin files

--- sdk/init.go
+++ sdk/init.go
@@ -1,15 +1,10 @@
 package sdk
 
 import (
-	"crypto/sha256"
 	"embed"
 	_ "embed"
-	"encoding/base64"
-	"fmt"
 	"github.com/ddkwork/golibrary/mylog"
 	"github.com/ddkwork/golibrary/stream"
-	"github.com/richardwilkes/toolbox/fatal"
-	"github.com/richardwilkes/toolbox/xio/fs"
 	"golang.org/x/sys/windows"
 	"os"
 	"path/filepath"
@@ -31,22 +26,22 @@ func init() {
 
 	SetCustomDriverPath(stringToBytePointer(dir), stringToBytePointer(stream.BaseName(dir)))
 
-	m.Range(func(k string, v []byte) bool { //copy sys to cache dir
+	m.Range(func(k string, v []byte) bool { //copy sys files to cache dir
 		stream.WriteTruncate(filepath.Join(dir, k), v)
 		return true
 	})
 
 	mylog.Check(windows.SetDllDirectory(dir)) //todo what another dep dll names ?
 
-	sha := sha256.Sum256(dllData)
-	dllName := fmt.Sprintf("libhyperdbg-%s.dll", base64.RawURLEncoding.EncodeToString(sha[:]))
-	filePath := filepath.Join(dir, dllName)
-	if !fs.FileExists(filePath) {
-		fatal.IfErr(os.WriteFile(filePath, dllData, 0644))
-	}
+	//sha := sha256.Sum256(dllData)
+	//dllName := fmt.Sprintf("libhyperdbg-%s.dll", base64.RawURLEncoding.EncodeToString(sha[:]))
+	//filePath := filepath.Join(dir, dllName)
+	//if !fs.FileExists(filePath) {
+	//	fatal.IfErr(os.WriteFile(filePath, dllData, 0644))
+	//}
 }
 
-func init() {
-	//windows.SetDllDirectory("../sdk.gen/SDK/Libraries")// abs not work,need temp dir
-	//SetDllDirectory("D:\\workspace\\workspace\\branch\\gui\\sdk.gen\\SDK\\Libraries")
-}
+//func init() {
+//windows.SetDllDirectory("../sdk.gen/SDK/Libraries")// abs not work,need temp dir
+//SetDllDirectory("D:\\workspace\\workspace\\branch\\gui\\sdk.gen\\SDK\\Libraries")
+//}
