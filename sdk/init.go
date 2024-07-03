07a540535fb0fdb41f79f9f950fdd95427f89ef9
Admin <2762713521@qq.com>
2024-07-03 22:55:13 +0800

    embed bin files

--- sdk/init.go
+++ sdk/init.go
@@ -23,13 +23,17 @@ func init() {
 	dir := mylog.Check2(os.UserCacheDir())
 	dir = filepath.Join(dir, "hyperdbg", "cache")
 	mylog.Check(os.MkdirAll(dir, 0755))
+
+	dllData := m.Get("libhyperdbg.dll")
+	GengoLibrary.LoadEmbed(dllData)
+
 	mylog.Check(windows.SetDllDirectory(dir))
 
-	sha := sha256.Sum256(m.Get("libhyperdbg.dll"))
+	sha := sha256.Sum256(dllData)
 	dllName := fmt.Sprintf("libhyperdbg-%s.dll", base64.RawURLEncoding.EncodeToString(sha[:]))
 	filePath := filepath.Join(dir, dllName)
 	if !fs.FileExists(filePath) {
-		fatal.IfErr(os.WriteFile(filePath, m.Get("libhyperdbg.dll"), 0644))
+		fatal.IfErr(os.WriteFile(filePath, dllData, 0644))
 	}
 }
 
