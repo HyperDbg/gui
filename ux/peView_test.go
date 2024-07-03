732e5152d0d591a17a87bad8ba139ba0e1b70891
Admin <2762713521@qq.com>
2024-07-04 04:40:09 +0800

    v0.0.4

--- ux/peView_test.go
+++ ux/peView_test.go
@@ -9,19 +9,19 @@ import (
 )
 
 func TestParserSys(t *testing.T) {
-	path := "hyperkd.sys"
+	path := "../sdk/bin/hyperkd.sys"
 	file := xed.ParserPe(path)
 	stream.MarshalJsonToFile(file, "hyperkd")
 }
 
 func TestParserExe(t *testing.T) {
-	path := "hyperdbg-cli.exe"
+	path := "../sdk/bin/hyperdbg-cli.exe"
 	file := xed.ParserPe(path)
 	stream.MarshalJsonToFile(file, "hyperdbg-cli")
 }
 
 func TestParserDll(t *testing.T) {
-	path := "msdia140.dll"
+	path := "../sdk/bin/msdia140.dll"
 	file := xed.ParserPe(path)
 	stream.MarshalJsonToFile(file, "msdia140")
 }
