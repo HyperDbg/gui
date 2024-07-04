47ce524cb74cf5dfd2faca2bf23674fb92439a91
Admin <2762713521@qq.com>
2024-07-04 18:01:01 +0800

    merge sdk directory

--- sdk/init.go
+++ sdk/init.go
@@ -19,10 +19,8 @@ func init() {
 	m := stream.ReadEmbedFileMap(data, "bin")
 	dir := mylog.Check2(os.UserCacheDir())
 	dir = filepath.Join(dir, "hyperdbg", "cache")
-
-	mylog.CheckIgnore(os.RemoveAll(dir))
+	mylog.Check(os.RemoveAll(dir))
 	mylog.Check(os.MkdirAll(dir, 0755))
-
 	m.Range(func(k string, v []byte) bool {
 		join := filepath.Join(dir, k)
 		if k == "hyperkd.sys" {
