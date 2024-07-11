c08dbed09f9899e43e2cc1f8624c7dd0fca2d71f
Admin <2762713521@qq.com>
2024-07-11 14:56:49 +0800

    bump app

--- ux/cpu.go
+++ ux/cpu.go
@@ -410,7 +410,6 @@ func LayoutDisassemblyTable() unison.Paneler {
 				root.AddChildByData(object)
 			}
 		case pe.ImageOptionalHeader64:
-			// todo if has tls replace oep
 			oepRVA := o.AddressOfEntryPoint
 			imageBase := o.ImageBase
 			oepVA := imageBase + uint64(oepRVA)
