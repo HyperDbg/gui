6adb90aa0222ea851d20a61bd6d876dde9576dd9
Admin <2762713521@qq.com>
2024-07-12 17:47:25 +0800

    fix LogView

--- ux/log.go
+++ ux/log.go
@@ -2,9 +2,10 @@ package ux
 
 import (
 	"github.com/ddkwork/app/widget"
+	"github.com/ddkwork/golibrary/mylog"
 	"github.com/richardwilkes/unison"
 )
 
 func LayoutLog() unison.Paneler {
-	return widget.NewLogView()
+	return widget.NewLogView(mylog.Body())
 }
