39696e906b33814510038d221691530ebee1ee0d
Admin <2762713521@qq.com>
2024-07-12 10:20:52 +0800

    fix https://github.com/HyperDbg/gui/issues/164

--- ux/doc.go
+++ ux/doc.go
@@ -1,11 +1,51 @@
 package ux
 
-import "github.com/richardwilkes/unison"
+import (
+	"fmt"
+	"github.com/ddkwork/app/widget"
+	"github.com/ddkwork/golibrary/mylog"
+	"github.com/richardwilkes/unison"
+)
 
-func LayoutDoc() unison.Border {
+func LayoutDoc() unison.Paneler {
 	type doc struct { // 功能分类和描述，实现与否，gif demo链接到别的仓库? 4个字段，同步更新readme
-		Title   string
-		Content string
+		Function    string
+		Description string
+		Implement   bool
+		DemoLink    string
 	}
-	return nil
+	table, header := widget.NewTable(doc{}, widget.TableContext[doc]{
+		ContextMenuItems: nil,
+		MarshalRow: func(node *widget.Node[doc]) (cells []widget.CellData) {
+			if node.Container() {
+				node.Sum("todo")
+			}
+			return []widget.CellData{
+				{Text: node.Data.Function},
+				{Text: node.Data.Description},
+				{Text: fmt.Sprintf("%t", node.Data.Implement)},
+				{Text: node.Data.DemoLink},
+			}
+		},
+		UnmarshalRow: func(node *widget.Node[doc], values []string) {
+			mylog.Todo("UnmarshalRow")
+		},
+		SelectionChangedCallback: func(root *widget.Node[doc]) {
+			mylog.Todo("SelectionChangedCallback")
+		},
+		SetRootRowsCallBack: func(root *widget.Node[doc]) {
+			for range 100 {
+				ts := doc{
+					Function:    "",
+					Description: "",
+					Implement:   false,
+					DemoLink:    "",
+				}
+				root.AddChildByData(ts)
+			}
+		},
+		JsonName:   "doc",
+		IsDocument: false, //todo
+	})
+	return widget.NewTableScrollPanel(table, header)
 }
