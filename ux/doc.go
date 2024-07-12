20804770576cd0c4e1e87d872a542150050eb22d
Admin <2762713521@qq.com>
2024-07-12 10:15:32 +0800

    fix https://github.com/HyperDbg/gui/issues/164

--- ux/doc.go
+++ ux/doc.go
@@ -1,1 +1,11 @@
 package ux
+
+import "github.com/richardwilkes/unison"
+
+func LayoutDoc() unison.Border {
+	type doc struct { // 功能分类和描述，实现与否，gif demo链接到别的仓库? 4个字段，同步更新readme
+		Title   string
+		Content string
+	}
+	return nil
+}
