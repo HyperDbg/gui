c9660b1bb29421ad640992157e02b3cf1807046e
Admin <2762713521@qq.com>
2024-07-22 01:38:23 +0800

    remove test

--- sdk/sdk_test.go
+++ sdk/sdk_test.go
@@ -24,46 +24,38 @@ func TestSdk(t *testing.T) {
 		fmt.Println("Received data:", BytePointerToString(text))
 		return 0
 	})
-	lib := mylog.Check2(syscall.LoadDLL("libhyperdbg.dll"))
-	procSetTextMessageCallback := mylog.Check2(lib.FindProc("hyperdbg_u_set_text_message_callback"))
-	mylog.Check3(procSetTextMessageCallback.Call(callback))
-
-	InterpreterEx("help !monitor")
-	return
-
-	//procSetTextMessageCallback := GengoLibrary.Import("hyperdbg_u_set_text_message_callback")
-	//procSetTextMessageCallback.Call1(callback)
 	SetTextMessageCallback(callback)
+	InterpreterEx("help !monitor")
 
-	mylog.Call(func() {
+	//mylog.Call(func() {
 
-		assert.True(t, VmxSupportDetection())
-		assert.True(t, SetCustomDriverPathEx(SysPath))
-		mylog.Trace("InstallVmmDriver", InstallVmmDriver())
+	assert.True(t, VmxSupportDetection())
+	assert.True(t, SetCustomDriverPathEx(SysPath))
+	mylog.Trace("InstallVmmDriver", InstallVmmDriver())
 
-		// ConnectLocalDebugger()
-		// assert.True(t, Boolean2Bool(ConnectCurrentDebuggerUsingComPort(StringToBytePointer("127.0.0.1"), 8080)))
-		// assert.True(t, Boolean2Bool(StartProcess(&[]rune("C:\\Windows\\SysWOW64\\notepad.exe")[0])))
+	// ConnectLocalDebugger()
+	// assert.True(t, Boolean2Bool(ConnectCurrentDebuggerUsingComPort(StringToBytePointer("127.0.0.1"), 8080)))
+	// assert.True(t, Boolean2Bool(StartProcess(&[]rune("C:\\Windows\\SysWOW64\\notepad.exe")[0])))
 
-		mylog.Trace("LoadVmm", LoadVmm())
-		// assert.True(t, Boolean2Bool(ConnectRemoteDebuggerUsingNamedPipe(StringToBytePointer("\\\\.\\pipe\\HyperDbgDebug"))))
+	mylog.Trace("LoadVmm", LoadVmm())
+	// assert.True(t, Boolean2Bool(ConnectRemoteDebuggerUsingNamedPipe(StringToBytePointer("\\\\.\\pipe\\HyperDbgDebug"))))
 
-		// assert.True(t, Boolean2Bool(StartProcessWithArgs(&[]rune("path")[0], &[]rune("C:\\Windows\\SysWOW64\\notepad.exe")[0])))
+	// assert.True(t, Boolean2Bool(StartProcessWithArgs(&[]rune("path")[0], &[]rune("C:\\Windows\\SysWOW64\\notepad.exe")[0])))
 
-		// todo:
-		// start debugger
-		// read memory
-		// read registers
-		// set breakpoints
-		// step over/into/out
-		// continue
-		// read stack
-		// stop debugger
+	// todo:
+	// start debugger
+	// read memory
+	// read registers
+	// set breakpoints
+	// step over/into/out
+	// continue
+	// read stack
+	// stop debugger
 
-		mylog.Trace("UnloadVmm", UnloadVmm())
-		mylog.Trace("StopVmmDriver", StopVmmDriver())
-		mylog.Trace("UninstallVmmDriver", UninstallVmmDriver())
-	})
+	mylog.Trace("UnloadVmm", UnloadVmm())
+	mylog.Trace("StopVmmDriver", StopVmmDriver())
+	mylog.Trace("UninstallVmmDriver", UninstallVmmDriver())
+	//})
 }
 
 /*
