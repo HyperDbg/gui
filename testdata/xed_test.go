381afe1012454803cd72e74cad2c4f1141f81f51
Admin <2762713521@qq.com>
2024-11-10 20:58:39 +0800

    init

---
+++ testdata/xed_test.go
@@ -0,0 +1,20 @@
+package xed_test
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/ddkwork/app/ms/xed"
+)
+
+//go:embed asm.exe
+var data []byte
+
+func TestDecode32(t *testing.T) {
+	x := xed.New(data).Decode32()
+	println(x.IntelSyntaxAsm.String())
+}
+
+func TestDecode64(t *testing.T) {
+	// Decode64(tt.args.data)
+}
