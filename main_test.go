8886e9a3138508974d0feaa4cab0cda9ce776dd7
Admin <2762713521@qq.com>
2025-03-05 18:28:23 +0800

    dev

--- main_test.go
+++ main_test.go
@@ -12,7 +12,17 @@ import (
 )
 
 func TestUpdateAppModule(t *testing.T) {
-	UpdateDependencies() // 执行一次提交一次，直到所有依赖都更新完毕，需要多次提交
+	paths := []string{
+		"D:\\workspace\\workspace\\demo\\toolbox",
+		"D:\\workspace\\workspace\\demo\\unison",
+		"D:\\workspace\\workspace\\demo\\app",
+	}
+	for _, path := range paths {
+		mylog.Check(os.Chdir(path))
+		UpdateDependencies() // 执行一次提交一次，直到所有依赖都更新完毕，需要多次提交
+	}
+	mylog.Check(os.Chdir("D:\\workspace\\workspace\\gui"))
+	stream.RunCommand("go get -x github.com/ddkwork/app@master")
 }
 
 func UpdateDependencies() {
@@ -25,7 +35,7 @@ func UpdateDependencies() {
 	 go get -x github.com/ddkwork/golibrary@master
 	 //go get -x github.com/ddkwork/ux@master
 	 go get -x github.com/google/go-cmp@master
-	 go get -x github.com/ddkwork/app@master
+	 //go get -x github.com/ddkwork/app@master
 	 go get -x github.com/ddkwork/toolbox@master
 	 go get -x github.com/ddkwork/unison@master
 	 //go get -x github.com/ebitengine/purego@main
