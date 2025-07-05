d9e2a8a1a2d4803bfdcd359da1dc1d1e3a3ada47
Admin <2762713521@qq.com>
2025-07-06 04:32:38 +0800

    dev

--- cmd.cmd
+++ cmd.cmd
@@ -15,3 +15,6 @@ git rev-parse HEAD
 git rev-parse HEAD
 go get -x github.com/ddkwork/golibrary@26b52a7347b5138ea92c20f3b0f39a6e1cef4f83
 go get -x github.com/ddkwork/ux@0dad9f4abbc1cd5e1017eaadcfd33efe86e2a6cb
+go mod tidy
+go run golang.org/x/tools/gopls/internal/analysis/modernize/cmd/modernize@latest -diff ./...
+go run golang.org/x/tools/gopls/internal/analysis/modernize/cmd/modernize@latest -fix ./...
