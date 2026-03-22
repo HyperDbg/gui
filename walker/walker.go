03d8f31e1a5902d2a1b70013a178a99a059eb846
Admin <2762713521@qq.com>
2026-03-23 06:28:31 +0800

    docs: 添加 Walker 接口 TODO 注释

--- walker/walker.go
+++ walker/walker.go
@@ -21,6 +21,7 @@ const (
 type Walker interface {
 	Maiter(event *debugger.DebugEvent, do func())
 	Hook(event *debugger.DebugEvent, do func())
+	//todo 通过 walker\winmd 解析pdb和sym文件，以及堆栈的api参数结构，数据类型，实现部分ida和ghidra功能
 }
 
 type ProcessBehaviorLogger struct {
